3eba26a142d3c5ee10eacfdb528c92ed51732546
xmeatballx <erik.jensen5@pcc.edu>
2024-06-12 17:01:44 -0400

    landing page hero initial commit

--- src/routes/(public)/+page.svelte
+++ src/routes/(public)/+page.svelte
@@ -1,22 +1,50 @@
 <script>
-	import { form, field } from 'svelte-forms';
-	import { required } from 'svelte-forms/validators';
-
-	const one = field('one', '', [required()]);
-	const two = field('two', '', [required()]);
-	const myForm = form(one, two);
+	import '@fontsource/archivo/900.css';
 </script>
 
-<section>
-	<select name="one" id="one" bind:value={$one.value}>
-		<option value="sdfa">Hi</option>
-		<option value="sdfSa">Hi</option>
-		<option value="sdfaa">Hi</option>
-		<option value="sddfa">Hi</option>
-	</select>
-	<select name="two" id="two" bind:value={$two.value}>
-		<option value="a">fsd</option>
-		<option value="s">dsd</option>
-	</select>
-	{JSON.stringify(myForm.summary())}
+<section class="container">
+	<div class="hero">
+		<div class="hero--content">
+			<h2>Use your Notion content to create custom embeddable widgets</h2>
+			<div class="button--wrapper">
+				<button><a href="/login">Get Started</a></button>
+			</div>
+		</div>
+		<iframe title="demo-one" src="http://localhost:3000/embed/clxc9qur90000ywqf8hjymcgj" />
+	</div>
 </section>
+
+<style>
+	.container {
+		max-width: 120ch;
+		margin: 0 auto;
+	}
+
+	.hero {
+		height: calc(100vh - 72px);
+		display: flex;
+		align-items: center;
+	}
+
+	.hero .hero--content {
+		display: grid;
+		gap: var(--size-4);
+		width: 60%;
+	}
+
+	.hero h2 {
+		font-family: 'Archivo';
+		font-size: var(--font-size-7);
+		font-weight: bolder;
+		margin-bottom: var(--size-2);
+		max-width: 30ch;
+	}
+
+	.hero iframe {
+		width: 50%;
+	}
+
+	.hero button {
+		background-color: var(--header);
+	}
+</style>
